7ae0e16aaaa69709963fd114763c91cf16b44a86
JanHoly <53258234+hongolem@users.noreply.github.com>
2022-04-11 22:37:09 +0200

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -14,52 +14,45 @@ function stred_soumernost() {
     B1 = [0, 1]
     C1 = [1, 1]
     D1 = [1, 0]
+    Plotting()
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    if (C1[0] - B1[0] == 3) {
+        Unplotting()
+        stred_soumernost()
+    } else {
+        Unplotting()
+        B1[1] += 1
+        D1[0] += 1
+        C1[0] += 1
+        C1[1] += 1
+        Plotting()
+    }
+    
+})
+function Plotting() {
     led.plot(A1[0], A1[1])
     led.plot(B1[0], B1[1])
     led.plot(C1[0], C1[1])
     led.plot(D1[0], D1[1])
-    A2 = [4 - A1[0], 4 - A1[1]]
-    B2 = [4 - B1[0], 4 - B1[1]]
-    C2 = [4 - C1[0], 4 - C1[1]]
-    D2 = [4 - D1[0], 4 - D1[1]]
+    let A2 = [4 - A1[0], 4 - A1[1]]
+    let B2 = [4 - B1[0], 4 - B1[1]]
+    let C2 = [4 - C1[0], 4 - C1[1]]
+    let D2 = [4 - D1[0], 4 - D1[1]]
     led.plot(A2[0], A2[1])
     led.plot(B2[0], B2[1])
     led.plot(C2[0], C2[1])
     led.plot(D2[0], D2[1])
 }
 
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-    if (C1[0] - B1[0] == 3) {
-        led.unplot(B1[0], B1[1])
-        led.unplot(C1[0], C1[1])
-        led.unplot(D1[0], D1[1])
-        led.unplot(4 - B1[0], 4 - B1[1])
-        led.unplot(4 - C1[0], 4 - C1[1])
-        led.unplot(4 - D1[0], 4 - D1[1])
-        stred_soumernost()
-    } else {
-        led.unplot(B1[0], B1[1])
-        led.unplot(C1[0], C1[1])
-        led.unplot(D1[0], D1[1])
-        led.unplot(4 - B1[0], 4 - B1[1])
-        led.unplot(4 - C1[0], 4 - C1[1])
-        led.unplot(4 - D1[0], 4 - D1[1])
-        B1[1] += 1
-        D1[0] += 1
-        C1[0] += 1
-        C1[1] += 1
-        led.plot(B1[0], B1[1])
-        led.plot(C1[0], C1[1])
-        led.plot(D1[0], D1[1])
-        A2 = [4 - A1[0], 4 - A1[1]]
-        B2 = [4 - B1[0], 4 - B1[1]]
-        C2 = [4 - C1[0], 4 - C1[1]]
-        D2 = [4 - D1[0], 4 - D1[1]]
-        led.plot(A2[0], A2[1])
-        led.plot(B2[0], B2[1])
-        led.plot(C2[0], C2[1])
-        led.plot(D2[0], D2[1])
-    }
-    
-})
+function Unplotting() {
+    led.unplot(B1[0], B1[1])
+    led.unplot(C1[0], C1[1])
+    led.unplot(D1[0], D1[1])
+    led.unplot(4 - B1[0], 4 - B1[1])
+    led.unplot(4 - C1[0], 4 - C1[1])
+    led.unplot(4 - D1[0], 4 - D1[1])
+}
+
